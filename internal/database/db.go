e98cc304724403ef1d1099e9c9aa872752b27770
jakkayy <niti2042547@gmail.com>
2026-08-16 23:36:12 +0700

    feat(db): set up PostgreSQL/SQLite database connection pool

---
+++ internal/database/db.go
@@ -0,0 +1,52 @@
+package database
+
+import (
+	"fmt"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"gorm.io/gorm/logger"
+)
+
+var DB *gorm.DB
+
+// InitDB initializes GORM database connection pool for SQLite or PostgreSQL
+func InitDB(dbType string, dsn string) (*gorm.DB, error) {
+	var dialector gorm.Dialector
+
+	switch dbType {
+	case "postgres":
+		dialector = postgres.Open(dsn)
+	case "sqlite":
+		if dsn == "" {
+			dsn = "envsync.db"
+		}
+		dialector = sqlite.Open(dsn)
+	default:
+		if dsn == "" {
+			dsn = "envsync.db"
+		}
+		dialector = sqlite.Open(dsn)
+	}
+
+	db, err := gorm.Open(dialector, &gorm.Config{
+		Logger: logger.Default.LogMode(logger.Warn),
+	})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to database (%s): %w", dbType, err)
+	}
+
+	sqlDB, err := db.DB()
+	if err != nil {
+		return nil, fmt.Errorf("failed to get sql.DB instance: %w", err)
+	}
+
+	sqlDB.SetMaxIdleConns(10)
+	sqlDB.SetMaxOpenConns(100)
+	sqlDB.SetConnMaxLifetime(time.Hour)
+
+	DB = db
+	return db, nil
+}
